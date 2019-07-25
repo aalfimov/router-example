7570252fc7ba2572954c8a9aaffaccd54270ee22
o_alfimov <steamcat089@gmail.com>
2019-07-25 09:52:21 +0300

    2 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PicturesDisplayComponent} from './photo-display/pictures-display.component';
+import {UploadPictureComponent} from './upload-picture/upload-picture.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pictures', component: PicturesDisplayComponent},
+  {path: 'upload', component: UploadPictureComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PicturesDisplayComponent } from './photo-display/pictures-display.component';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PicturesDisplayComponent,
+    UploadPictureComponent
   ],
   imports: [
     BrowserModule,
