6e5d2bd445b027991b6ca8c792066c6ceab160fd
o_alfimov <steamcat089@gmail.com>
2019-07-29 16:43:23 +0300

    todo add file reader

--- src/app/upload-picture/upload-picture.component.ts
+++ src/app/upload-picture/upload-picture.component.ts
@@ -10,6 +10,23 @@ export class UploadPictureComponent implements OnInit {
 
   constructor(private picturesService: PicturesService) {
   }
+  changeListener($event): void {
+    this.readThis($event.target);
+  }
+  // https://developer.mozilla.org/ru/docs/Web/API/FileReader
+  // https://ru.wikipedia.org/wiki/Base64
+  // ToDo download pictures and conver them to base64
+
+  readThis(inputValue: any): void {
+    const file: File = inputValue.files[0];
+    const myReader: FileReader = new FileReader();
+    const fileType = inputValue.parentElement.id;
+    myReader.onloadend = (e) => {
+      console.log(myReader.result);
+    };
+
+    myReader.readAsText(file);
+  }
 
   ngOnInit() {
   }
