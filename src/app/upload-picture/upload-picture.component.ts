edd2ee7cdc2da0dabdba2650745ed51f73105089
o_alfimov <steamcat089@gmail.com>
2019-07-30 14:06:40 +0300

    simple changes

--- src/app/upload-picture/upload-picture.component.ts
+++ src/app/upload-picture/upload-picture.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {PicturesService} from '../pictures.service';
 
 @Component({
@@ -6,33 +6,26 @@ import {PicturesService} from '../pictures.service';
   templateUrl: './upload-picture.component.html',
   styleUrls: ['./upload-picture.component.scss']
 })
-export class UploadPictureComponent implements OnInit {
+export class UploadPictureComponent {
   private base64textString: string;
 
   constructor(private picturesService: PicturesService) {
   }
 
   handleFileSelect(evt) {
-    const files = evt.target.files;
-    const file = files[0];
-
-    if (files && file) {
+    const file = evt.target.files[0];
+    if (file) {
       const reader = new FileReader();
-
       reader.onload = this.handleReaderLoaded.bind(this);
-
       reader.readAsBinaryString(file);
+      console.log();
     }
   }
 
   handleReaderLoaded(readerEvt) {
-    console.log(readerEvt.target);
     const binaryString = readerEvt.target.result;
     this.base64textString = btoa(binaryString);
     this.add('data:image/jpg;base64,' + this.base64textString);
-  }
-
-  ngOnInit() {
   }
 
   add(value: string) {
