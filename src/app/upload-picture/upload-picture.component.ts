45826c24429e7842e935b906da1f1b520c502c34
o_alfimov <steamcat089@gmail.com>
2019-08-02 13:49:03 +0300

    fix validation

--- src/app/upload-picture/upload-picture.component.ts
+++ src/app/upload-picture/upload-picture.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PicturesService} from '../pictures.service';
-import {FormBuilder, FormControl, FormGroup, ValidationErrors} from '@angular/forms';
+import {FormBuilder, FormGroup, ValidationErrors} from '@angular/forms';
 
 @Component({
   selector: 'app-upload-picture',
@@ -11,45 +11,28 @@ export class UploadPictureComponent implements OnInit {
 
   constructor(private picturesService: PicturesService, private fb: FormBuilder) {
   }
+
   imgURL: string;
   public imageForm: FormGroup;
 
   ngOnInit(): void {
     this.initForm();
   }
+
   private initForm() {
     this.imageForm = this.fb.group({
       url: [''],
       file: ['']
     }, {validators: this.myCustomValidator});
   }
-  // , this.requiredFileType('png')
-  // requiredFileType( type: string ) {
-  //   return (control: FormControl) => {
-  //     const file = control.value;
-  //     if ( file ) {
-  //       const extension = file.name.split('.')[1].toLowerCase();
-  //       if ( type.toLowerCase() !== extension.toLowerCase() ) {
-  //         return {
-  //           requiredFileType: true
-  //         };
-  //       }
-  //       return null;
-  //     }
-  //     return null;
-  //   };
-  // }
 
   myCustomValidator(control: FormGroup): ValidationErrors | null {
-    const url = control.get('url');
-    const file = control.get('file');
-    return (url.value.length || file.value.length) ? null :  {required : true};
+    const url = control. get('url').value.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:jp(e?)g|gif|png)/) != null;
+    const file = control.get('file').value.match(/(?:jp(e?)g|gif|png)/) != null;
+    return ( url || file) ? null : {required: true};
   }
-  setPreviewUrl(value: string) {
-    this.imgURL = value;
-  }
+
   handleFileSelect(event) {
-    console.log('event' + event.target);
     this.imgURL = '';
     const file = event.target.files[0];
     if (file.type.match(/image\/*/) != null) {
@@ -58,13 +41,16 @@ export class UploadPictureComponent implements OnInit {
       reader.readAsDataURL(file);
     }
   }
+
   handleReaderLoaded(readerEvent) {
     this.imgURL = readerEvent.target.result;
-    this.imageForm.controls.file.setValue(this.imgURL ? this.imgURL : null);
   }
 
-  add(event) {
-    console.log(event);
-    this.picturesService.add(this.imgURL);
+  add() {
+    if (this.imageForm.value.url) {
+      this.picturesService.add(this.imageForm.value.url);
+    } else {
+      this.picturesService.add(this.imgURL);
+    }
   }
 }
