4639974f72a9d3d3c1e18fcd6d5926fbdfdbea1c
o_alfimov <steamcat089@gmail.com>
2019-08-06 15:19:30 +0300

    fix validators

--- src/app/upload-picture/upload-picture.component.ts
+++ src/app/upload-picture/upload-picture.component.ts
@@ -8,7 +8,6 @@ import {FormBuilder, FormGroup, ValidationErrors, Validators} from '@angular/for
   styleUrls: ['./upload-picture.component.scss']
 })
 export class UploadPictureComponent implements OnInit {
-
   constructor(private picturesService: PicturesService, private fb: FormBuilder) {
   }
 
@@ -21,30 +20,28 @@ export class UploadPictureComponent implements OnInit {
 
   private initForm() {
     this.imageForm = this.fb.group({
-      url: ['', this.urlCustomValidator],
-      file: ['']
-    }, {validators: this.myCustomValidator});
+      url: ['', this.urlValidator],
+      file: ['', this.fileValidator]
+    }, {validators: this.myCheckValidator});
   }
 
-  urlCustomValidator(control: FormGroup): ValidationErrors | null {
+  urlValidator(control: FormGroup): ValidationErrors | null {
     const url = control.value.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:jp(e?)g|gif|png)/) != null;
     return (url) ? null : {required: true};
   }
-  //
-  // fileCustomValidator(control: FormGroup): ValidationErrors | null {
-  //     console.log(control.value);
-  //   const file = control.get('file').value.match(/(?:jp(e?)g|gif|png)/) != null;
-  //   return (file) ? null : {required: true};
-  // }
 
-  myCustomValidator(control: FormGroup): ValidationErrors | null {
-    const url = control.value.url != null; // .match(/(http(s?):)([/|.|\w|\s|-])*\.(?:jp(e?)g|gif|png)/) != null;
-    const file = control.value.file.match(/(?:jp(e?)g|gif|png)/) != null;
+  fileValidator(control: FormGroup): ValidationErrors | null {
+    const file = control.value.match(/(?:jp(e?)g|gif|png)/) != null;
+    return (file) ? null : {required: true};
+  }
+
+  myCheckValidator(control: FormGroup): ValidationErrors | null {
+    const url = control.value.url != null;
+    const file = control.value.file != null;
     return (url || file) ? null : {required: true};
   }
 
   handleFileSelect(event) {
-    this.imageForm.value.url = '';
     const file = event.target.files[0];
     if (file.type.match(/image\/*/) != null) {
       const reader = new FileReader();
@@ -55,23 +52,10 @@ export class UploadPictureComponent implements OnInit {
 
   handleReaderLoaded(readerEvent) {
     this.imgURL = readerEvent.target.result;
-    this.imageForm.value.file = readerEvent.target.result;
   }
 
-  // add() {
-  //   this.picturesService.add(this.imgURL);
-  //   // if (this.imageForm.value.url) {
-  //   //   this.picturesService.add(this.imageForm.value.url);
-  //   // }
-  //   // if (this.imageForm.value.file) {
-  //   //   this.picturesService.add(this.imgURL);
-  //   // }
-  // }
-
   clearFileInput() {
-    this.imgURL = '';
-    this.imageForm.value.file = '';
-    if (this.imageForm.valid && this.imageForm.controls.url.valid) {
+    if (this.imageForm.controls.url.valid) {
       this.imgURL = this.imageForm.value.url;
     }
   }
